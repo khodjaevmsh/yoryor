5032a95ff03b415489941cecf7fd200cb2b65d13
khodjaevmsh <khodjaevmsh@gmail.com>
2024-08-08 19:13:58 +0500

    improve websocket files

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -7,13 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-# import os
-# from django.core.asgi import get_asgi_application
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-# application = get_asgi_application()
-
-import os
-
+from .wsgi import *  # add this line to top of your code
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
